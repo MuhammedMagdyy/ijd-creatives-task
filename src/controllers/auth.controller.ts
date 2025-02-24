130087727f802bb629b9604ed22cd16e6bbde184
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-25 01:38:45 +0200

    refactor auth and order controllers; enhance registration, login, and order management with new methods & response handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,40 +3,39 @@ import asyncHandler from 'express-async-handler';
 import { authService } from '../services';
 import { loginSchema, registerSchema, verifyOtpSchema } from '../utils';
 
-export const handleRegister = asyncHandler(
+export const processUserRegistration = asyncHandler(
   async (req: Request, res: Response) => {
     const { name, email, password, phone } = registerSchema.parse(req.body);
 
-    const user = await authService.register(name, email, password, phone);
+    await authService.register({ name, email, password, phone });
 
     res.status(201).json({
-      message: 'User created successfully',
-      data: {
-        user: user.registeredUser,
-      },
-      token: user.token,
+      message: 'User created successfully, please verify your email',
+      data: null,
     });
   }
 );
 
-export const handleLogin = asyncHandler(async (req: Request, res: Response) => {
-  const { email, password } = loginSchema.parse(req.body);
+export const processUserLogin = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { email, password } = loginSchema.parse(req.body);
 
-  const token = await authService.login(email, password);
+    const loggedInUser = await authService.login({ email, password });
 
-  res.status(200).json({
-    message: 'Logged in successfully',
-    data: {},
-    token,
-  });
-});
+    res.status(200).json({
+      message: 'Logged in successfully',
+      data: loggedInUser.user,
+      tokens: loggedInUser.tokens,
+    });
+  }
+);
 
-export const handleVerifyOtp = asyncHandler(
+export const processOtpVerification = asyncHandler(
   async (req: Request, res: Response) => {
     const { email, otp } = verifyOtpSchema.parse(req.body);
 
     await authService.verifyOtp(email, otp);
 
-    res.status(200).json({ message: 'User verified successfully', data: {} });
+    res.status(200).json({ message: 'User verified successfully', data: null });
   }
 );
--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,27 +1,88 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import { orderService } from '../services';
-import { createOrder, paginationSchema } from '../utils';
+import {
+  orderDetailsSchema,
+  paginationSchema,
+  orderPatchSchema,
+  paramsSchema,
+} from '../utils';
 
-export const handleOrder = asyncHandler(async (req: Request, res: Response) => {
-  const userId = req.user?.id as number;
-
-  const { details, price } = createOrder.parse(req.body);
-
-  const order = await orderService.createOne(details, price, userId);
-
-  res.status(201).json({ message: 'Order created successfully', data: order });
-});
-
-export const handleOrderWithPagination = asyncHandler(
+export const processOrderRequest = asyncHandler(
   async (req: Request, res: Response) => {
-    const { pageSize, pageNumber } = paginationSchema.parse(req.query);
+    const userId = req.user?.id as number;
 
-    const orders = await orderService.findAllWithPagination(
-      +pageSize as number,
-      +pageNumber as number
+    const { details, price } = orderDetailsSchema.parse(req.body);
+
+    const order = await orderService.createOne(details, price, userId);
+
+    res
+      .status(201)
+      .json({ message: 'Order created successfully', data: order });
+  }
+);
+
+export const getPaginatedOrders = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { pageNumber, pageSize } = paginationSchema.parse(req.query);
+
+    const orders = await orderService.retrieveOrdersWithPagination({
+      pageNumber,
+      pageSize,
+    });
+
+    res
+      .status(200)
+      .json({ message: 'Orders retrieved successfully', data: orders });
+  }
+);
+
+export const processFullOrderUpdate = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id: orderId } = paramsSchema.parse(req.params);
+    const { id: userId } = paramsSchema.parse(req.user);
+
+    const { details, price } = orderDetailsSchema.parse(req.body);
+
+    const order = await orderService.updateOrderById(
+      orderId,
+      userId,
+      details,
+      price
     );
 
-    res.status(200).json({ message: 'Orders', data: orders });
+    res
+      .status(200)
+      .json({ message: 'Order updated successfully', data: order });
+  }
+);
+
+export const processPartialOrderUpdate = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id: orderId } = paramsSchema.parse(req.params);
+    const { id: userId } = paramsSchema.parse(req.user);
+
+    const { details, price } = orderPatchSchema.parse(req.body);
+
+    const order = await orderService.updateOrderById(
+      orderId,
+      userId,
+      details,
+      price
+    );
+
+    res
+      .status(200)
+      .json({ message: 'Order partially updated successfully', data: order });
+  }
+);
+
+export const processOrderDeletion = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id: orderId } = paramsSchema.parse(req.params);
+
+    await orderService.deleteOrderById(orderId);
+
+    res.status(204).send();
   }
 );
