7d160328960c948c6f4a463c01e7f58bbdb29f76
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-24 17:59:19 +0200

    add not found error handling for undefined routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import orderRoutes from './order.routes';
-import authRoutes from './auth.routes';
+import { authRoutes } from './auth.routes';
+import { orderRoutes } from './order.routes';
 import { isAuth } from '../middlewares';
+import { ApiError } from '../utils';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/orders', isAuth, orderRoutes);
+
+router.all('*', (req, _res, next) => {
+  return next(
+    new ApiError(`Are you lost? There's nothing here ${req.originalUrl}`, 404)
+  );
+});
 
 export default router;
