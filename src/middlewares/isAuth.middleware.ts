aacdc9fa9b794998c4fa67246a465e4c1eec4701
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-25 01:34:05 +0200

    add isAdmin middleware for admin authority, and wrapped isAuth middleware by asyncHandler

--- src/middlewares/isAuth.middleware.ts
+++ src/middlewares/isAuth.middleware.ts
@@ -4,6 +4,7 @@ import { Secret, verify } from 'jsonwebtoken';
 import { ApiError } from '../utils';
 import { IJwtPayload } from '../interfaces';
 import { userService } from '../services';
+import { UserRole } from '@prisma/client';
 
 export const isAuth = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -29,6 +30,22 @@ export const isAuth = asyncHandler(
     }
 
     req.user = { id: decoded.id };
+
+    next();
+  }
+);
+
+export const isAdmin = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      throw new ApiError('Unauthorized', 401);
+    }
+
+    const user = await userService.findOne({ id: req.user.id });
+
+    if (!user || user.role !== UserRole.ADMIN) {
+      throw new ApiError('You are not authorized', 403);
+    }
 
     next();
   }
